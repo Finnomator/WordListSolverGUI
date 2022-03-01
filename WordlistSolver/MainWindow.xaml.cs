ed733e82620f34c524134fac24371d8ce76ed7ad
Finnomator <finndruenert@gmail.com>
2022-03-01 12:00:23 +0100

    init

---
+++ WordlistSolver/MainWindow.xaml.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WordlistSolver
+{
+
+    public class MyLabel
+    {
+        public TextBox label;
+        public MyLabel(string content, int x, int y, int fontsize)
+        {
+            label = new TextBox();
+            label.Height = Double.NaN;
+            label.Width = Double.NaN;
+            label.FontSize = fontsize;
+            label.FontFamily = new System.Windows.Media.FontFamily("Segoe Ui");
+            label.IsReadOnly = true;
+            label.Background = null;
+            label.BorderBrush = null;
+            label.BorderThickness = new Thickness(0, 0, 0, 0);
+
+            label.Margin = new Thickness(x, y, 0, 0);
+            label.Padding = new Thickness(0, 0, 0, 0);
+
+            label.HorizontalAlignment = HorizontalAlignment.Left;
+            label.VerticalAlignment = VerticalAlignment.Top;
+            label.Text = content;
+
+        }
+    }
+
+    public class MyButton
+    {
+        public Button button;
+
+        public MyButton(string text, int x, int y, int fontsize)
+        {
+            button = new Button();
+            button.Height = fontsize;
+            button.Width = 70;
+            //button.FontSize = fontsize;
+            button.Name = int.Parse(text.Split(' ')[1]).ToString();
+            button.FontFamily = new FontFamily("Segoe Ui");
+            button.BorderThickness = new Thickness(0, 0, 0, 0);
+            button.Background = new SolidColorBrush(Color.FromRgb(179, 194, 242));
+
+            button.Margin = new Thickness(0, y, x, 0);
+            button.Padding = new Thickness(-5, -5.5, -5, -5);
+
+            button.HorizontalAlignment = HorizontalAlignment.Right;
+            button.VerticalAlignment = VerticalAlignment.Top;
+            button.Content = text;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        bool first = true;
+        bool first2 = true;
+        int copy_num = -1;
+        list_compare wc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainGrid.Children.Remove(Scroller);
+            wc = new list_compare();
+        }
+        void make_output()
+        {
+
+            if (!(bool)UseLabelsCheck.IsChecked)
+            {
+
+                if (WordInputTextBox.Text.Length < 3)
+                {
+                    OutputTextBox.Text = "";
+                    return;
+                }
+
+                List<string> posible_words;
+
+                if (copy_num == -1)
+                {
+                    posible_words = wc.wordlist(WordInputTextBox.Text, !(bool)AttentionSpace.IsChecked);
+                }
+                else
+                {
+                    posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), !(bool)AttentionSpace.IsChecked);
+                    if (copy_num > posible_words.Count || copy_num < 1)
+                    {
+                        return;
+                    }
+                    Clipboard.SetText(posible_words[copy_num - 1]);
+                    return;
+                }
+
+                OutputTextBox.Text = "";
+                if ((bool)ReplaceHyphonWith_.IsChecked)
+                {
+                    int oldpos = WordInputTextBox.SelectionStart;
+                    WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
+                    WordInputTextBox.SelectionStart = oldpos;
+                }
+
+                if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
+                {
+                    Clipboard.SetText(posible_words[0]);
+                }
+
+                int digits = posible_words.Count.ToString().Length;
+
+                for (int i = 0; i < posible_words.Count; i++)
+                {
+                    string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
+                    OutputTextBox.AppendText((i + 1) + ". " + space + posible_words[i] + Environment.NewLine);
+                }
+            }
+
+            else
+            {
+                if (WordInputTextBox.Text.Length < 3)
+                {
+                    LabelGrid.Children.Clear();
+                    return;
+                }
+
+                List<string> posible_words;
+
+                if (copy_num == -1)
+                {
+                    posible_words = wc.wordlist(WordInputTextBox.Text, !(bool)AttentionSpace.IsChecked);
+                }
+                else
+                {
+                    posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), !(bool)AttentionSpace.IsChecked);
+                    if (copy_num > posible_words.Count || copy_num < 1)
+                    {
+                        return;
+                    }
+                    Clipboard.SetText(posible_words[copy_num - 1]);
+                    return;
+                }
+
+                LabelGrid.Children.Clear();
+                if ((bool)ReplaceHyphonWith_.IsChecked)
+                {
+                    int oldpos = WordInputTextBox.SelectionStart;
+                    WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
+                    WordInputTextBox.SelectionStart = oldpos;
+                }
+
+                if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
+                {
+                    Clipboard.SetText(posible_words[0]);
+                }
+
+                int digits = posible_words.Count.ToString().Length;
+
+                for (int i = 0; i < posible_words.Count; i++)
+                {
+
+                    int x = 0;
+                    int fontsize = int.Parse(FontSizeBox.Text);
+                    int y = i * (fontsize + 3);
+
+                    string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
+                    MyLabel mylabel = new MyLabel((i + 1) + ". " + space + posible_words[i], x, y, fontsize);
+                    LabelGrid.Children.Add(mylabel.label);
+
+                    MyButton copybutton = new MyButton("Copy " + (i + 1) + ".", 30, y, fontsize);
+
+                    LabelGrid.Children.Add(copybutton.button);
+                }
+            }
+        }
+
+
+        int extract_num_fromstring(string theString)
+        {
+            bool foundnum = false;
+            int i = 0;
+            for (; i < theString.Length; i++)
+            {
+                if (char.IsNumber(theString[i]))
+                {
+                    foundnum = true;
+                    break;
+                }
+            }
+
+            if (foundnum)
+            {
+                int end = theString.Length - i;
+                int num;
+                if (int.TryParse(theString.Substring(i, end), out num))
+                {
+                    return num;
+                }
+
+            }
+
+            return -1;
+        }
+
+        private async void InputChange(object sender, TextChangedEventArgs e)
+        {
+
+            if (!first)
+            {
+                int wlength = 0;
+
+                for (int i = 0; i < WordInputTextBox.Text.Length; i++)
+                {
+                    if (!int.TryParse(WordInputTextBox.Text[i].ToString(), out int _))
+                    {
+                        wlength++;
+                    }
+                }
+
+                WordLengthLabel.Content = "Word length: " + wlength;
+            }
+            else
+            {
+                first = false;
+            }
+
+            async Task<bool> UserKeepsTyping()
+            {
+                string txt = WordInputTextBox.Text;
+                if (int.TryParse(TextUpdateTimeBox.Text, out int n))
+                {
+                    await Task.Delay(n);
+                }
+                else
+                {
+                    await Task.Delay(100);
+                    TextUpdateTimeBox.Text = "100";
+                }
+
+                return txt != WordInputTextBox.Text;
+            }
+            if (await UserKeepsTyping()) return;
+
+            copy_num = extract_num_fromstring(WordInputTextBox.Text);
+
+            make_output();
+        }
+
+        private void CheckBoxCheck(object sender, RoutedEventArgs e)
+        {
+            make_output();
+        }
+
+        private void FontSizeChange(object sender, TextChangedEventArgs e)
+        {
+
+            if (int.TryParse(FontSizeBox.Text, out int n))
+            {
+                OutputTextBox.FontSize = n;
+            }
+            else
+            {
+                if (FontSizeBox.Text == "")
+                {
+                    OutputTextBox.FontSize = 16;
+                }
+                else
+                {
+                    FontSizeBox.Text = "16";
+                }
+            }
+
+        }
+
+        private void UseLabelsClick(object sender, RoutedEventArgs e)
+        {
+            if ((bool)UseLabelsCheck.IsChecked)
+            {
+                MainGrid.Children.Remove(OutputTextBox);
+                MainGrid.Children.Add(Scroller);
+            }
+            else
+            {
+                MainGrid.Children.Add(OutputTextBox);
+                MainGrid.Children.Remove(Scroller);
+            }
+
+            make_output();
+        }
+
+        private void ClickCopyButton()
+        {
+
+        }
+    }
+}
---
+++ WordlistSolver/list_compare.cs
@@ -0,0 +1,76 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+public class list_compare
+{
+    wordlist WordList;
+    public list_compare()
+    {
+        WordList = new wordlist();
+    }
+
+    List<List<char>> get_wordlist_of_list()
+    {
+        List<List<char>> new_words = new List<List<char>>();
+        string[] words = WordList.words;
+        for (int i = 0; i < words.Length; i++)
+        {
+            new_words.Add(new List<char>());
+            foreach (char l in words[i])
+            {
+                new_words[i].Add(char.ToLower(l));
+            }
+
+        }
+
+        return new_words;
+    }
+
+    public List<string> wordlist(string request, bool allow_spaces = true)
+    {
+        List<List<char>> word_list = get_wordlist_of_list();
+        int length = request.Length;
+        request = request.ToLower();
+        char[] request_list = request.ToCharArray();
+
+        List<List<char>> approved = new List<List<char>>();
+        foreach (List<char> i in word_list)
+        {
+            if (length == i.Count)
+            {
+                approved.Add(i);
+            }
+        }
+
+        List<string> super_approved = new List<string>();
+        foreach (List<char> i in approved)
+        {
+            bool fitting = true;
+            for (int j = 0; j < length; j++)
+            {
+                if (request_list[j] != '_' && request_list[j] != i[j])
+                {
+                    fitting = false;
+                    break;
+                }
+            }
+            if (fitting)
+            {
+                if (allow_spaces)
+                {
+                    super_approved.Add(string.Join("", i));
+                }
+                else
+                {
+                    if (!i.Contains(' ') || Array.IndexOf(request_list, ' ') > -1)
+                    {
+                        super_approved.Add(string.Join("", i));
+                    }
+                }
+            }
+        }
+        return super_approved;
+    }
+}
