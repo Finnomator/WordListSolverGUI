8dd5f89ec6e3e75e5145225f813f7ddcd983c101
Finnomator <finndruenert@gmail.com>
2022-03-17 17:15:24 +0100

    added anti 3secs

--- WordlistSolver/MainWindow.xaml.cs
+++ WordlistSolver/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Threading;
+using System.Windows.Input;
 
 namespace WordlistSolver
 {
@@ -43,7 +45,6 @@ namespace WordlistSolver
             button = new Button();
             button.Height = fontsize;
             button.Width = 70;
-            //button.FontSize = fontsize;
             button.Name = name.Replace(" ", "_").Replace("-", "__");
             button.Click += handler;
             button.FontFamily = new FontFamily("Segoe Ui");
@@ -62,126 +63,206 @@ namespace WordlistSolver
     public partial class MainWindow : Window
     {
         bool first = true;
-        bool first2 = true;
         int copy_num = -1;
         list_compare wc;
+        private volatile int cooldown = 0;
+
         public MainWindow()
         {
+
+
             InitializeComponent();
+
             MainGrid.Children.Remove(Scroller);
             wc = new list_compare();
+
+            Thread enterdetector = new Thread(Detect_Enter);
+            enterdetector.SetApartmentState(ApartmentState.STA);
+            enterdetector.IsBackground = true;
+            enterdetector.Start();
+
+            Thread counter = new Thread(() => count_down());
+            counter.SetApartmentState(ApartmentState.STA);
+            counter.IsBackground = true;
+            counter.Start();
+
         }
-        void make_output()
+
+
+        void label_output()
         {
-
             bool endless_word = WordInputTextBox.Text.StartsWith(":");
-
-            if (!(bool)UseLabelsCheck.IsChecked)
+            if ((WordInputTextBox.Text.Length < 3 && !endless_word) || (WordInputTextBox.Text.Length < 2))
             {
 
-                if ((WordInputTextBox.Text.Length < 3 && !endless_word) || (WordInputTextBox.Text.Length < 2))
-                {
-
-                    OutputTextBox.Text = "";
-                    return;
-                }
-
-                List<string> posible_words;
-
-                if (copy_num == -1)
-                {
-                    posible_words = wc.wordlist(WordInputTextBox.Text, endless_word, !(bool)AttentionSpace.IsChecked);
-                }
-                else
-                {
-                    posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), endless_word, !(bool)AttentionSpace.IsChecked);
-                    if (copy_num > posible_words.Count || copy_num < 1)
-                    {
-                        return;
-                    }
-                    Clipboard.SetText(posible_words[copy_num - 1]);
-                    return;
-                }
-
                 OutputTextBox.Text = "";
-                if ((bool)ReplaceHyphonWith_.IsChecked)
-                {
-                    int oldpos = WordInputTextBox.SelectionStart;
-                    WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
-                    WordInputTextBox.SelectionStart = oldpos;
-                }
+                return;
+            }
 
-                if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
-                {
-                    Clipboard.SetText(posible_words[0]);
-                }
+            List<string> posible_words;
 
-                int digits = posible_words.Count.ToString().Length;
-
-                for (int i = 0; i < posible_words.Count; i++)
+            if (copy_num == -1)
+            {
+                posible_words = wc.wordlist(WordInputTextBox.Text, endless_word, !(bool)AttentionSpace.IsChecked);
+            }
+            else
+            {
+                posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), endless_word, !(bool)AttentionSpace.IsChecked);
+                if (copy_num > posible_words.Count || copy_num < 1)
                 {
-                    string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
-                    OutputTextBox.AppendText((i + 1) + ". " + space + posible_words[i] + Environment.NewLine);
+                    return;
                 }
+                Clipboard.SetText(posible_words[copy_num - 1]);
+                return;
+            }
+
+            OutputTextBox.Text = "";
+            if ((bool)ReplaceHyphonWith_.IsChecked)
+            {
+                int oldpos = WordInputTextBox.SelectionStart;
+                WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
+                WordInputTextBox.SelectionStart = oldpos;
+            }
+
+            if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
+            {
+                Clipboard.SetText(posible_words[0]);
+            }
+
+            int digits = posible_words.Count.ToString().Length;
+
+            for (int i = 0; i < posible_words.Count; i++)
+            {
+                string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
+                OutputTextBox.AppendText((i + 1) + ". " + space + posible_words[i] + Environment.NewLine);
+            }
+        }
+
+
+        void normal_output()
+        {
+            bool endless_word = WordInputTextBox.Text.StartsWith(":");
+            if ((WordInputTextBox.Text.Length < 3 && !endless_word) || (WordInputTextBox.Text.Length < 2))
+            {
+
+                LabelGrid.Children.Clear();
+                return;
+            }
+
+
+            List<string> posible_words;
+
+            if (copy_num == -1)
+            {
+                posible_words = wc.wordlist(WordInputTextBox.Text, endless_word, !(bool)AttentionSpace.IsChecked);
+            }
+            else
+            {
+                posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), endless_word, !(bool)AttentionSpace.IsChecked);
+                if (copy_num > posible_words.Count || copy_num < 1)
+                {
+                    return;
+                }
+                Clipboard.SetText(posible_words[copy_num - 1]);
+                return;
+            }
+
+            LabelGrid.Children.Clear();
+            if ((bool)ReplaceHyphonWith_.IsChecked)
+            {
+                int oldpos = WordInputTextBox.SelectionStart;
+                WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
+                WordInputTextBox.SelectionStart = oldpos;
+            }
+
+            if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
+            {
+                Clipboard.SetText(posible_words[0]);
+            }
+
+            int digits = posible_words.Count.ToString().Length;
+
+            for (int i = 0; i < posible_words.Count; i++)
+            {
+
+                int x = 0;
+                int fontsize = int.Parse(FontSizeBox.Text);
+                int y = i * (fontsize + 3);
+
+                string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
+                MyLabel mylabel = new MyLabel((i + 1) + ". " + space + posible_words[i], x, y, fontsize);
+                LabelGrid.Children.Add(mylabel.label);
+
+                MyButton copybutton = new MyButton("Copy " + (i + 1) + ".", 30, y, fontsize, ClickCopyButton, posible_words[i]);
+
+                LabelGrid.Children.Add(copybutton.button);
+            }
+        }
+
+
+        void make_output()
+        {
+            if (!(bool)UseLabelsCheck.IsChecked)
+            {
+                label_output();
             }
 
             else
             {
+                normal_output();
+            }
+        }
 
-                if ((WordInputTextBox.Text.Length < 3 && !endless_word) || (WordInputTextBox.Text.Length < 2))
+
+        void Detect_Enter()
+        {
+            while (true)
+            {
+                if ((Keyboard.GetKeyStates(Key.Enter) & KeyStates.Down) > 0)
                 {
+                    cooldown = 3000;
 
-                    LabelGrid.Children.Clear();
-                    return;
-                }
-
-
-                List<string> posible_words;
-
-                if (copy_num == -1)
-                {
-                    posible_words = wc.wordlist(WordInputTextBox.Text, endless_word, !(bool)AttentionSpace.IsChecked);
-                }
-                else
-                {
-                    posible_words = wc.wordlist(WordInputTextBox.Text.Replace(copy_num.ToString(), ""), endless_word, !(bool)AttentionSpace.IsChecked);
-                    if (copy_num > posible_words.Count || copy_num < 1)
+                    while ((Keyboard.GetKeyStates(Key.Enter) & KeyStates.Down) > 0)
                     {
-                        return;
+                        Thread.Sleep(50);
                     }
-                    Clipboard.SetText(posible_words[copy_num - 1]);
-                    return;
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+
+        void count_down(int stepsize = 100)
+        {
+            // Stepsizes under 100 are very unprecise
+            while (true)
+            {
+                while (cooldown <= 0)
+                {
+                    Thread.Sleep(50);
                 }
 
-                LabelGrid.Children.Clear();
-                if ((bool)ReplaceHyphonWith_.IsChecked)
+                int B = 0;
+                int G = 0;
+                int R = 255;
+
+                for (; cooldown >= 0; cooldown -= stepsize)
                 {
-                    int oldpos = WordInputTextBox.SelectionStart;
-                    WordInputTextBox.Text = WordInputTextBox.Text.Replace("-", "_");
-                    WordInputTextBox.SelectionStart = oldpos;
-                }
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        EnterCooldownLabeltime.Content = Math.Round(cooldown / 1000.0, stepsize.ToString().Length) + "s";
+                        EnterCooldownLabel.Background = new SolidColorBrush(Color.FromArgb(204, (byte)R, (byte)G, (byte)B));
+                    });
+                    Thread.Sleep(stepsize);
 
-                if (posible_words.Count == 1 && (bool)AutoCopyBox.IsChecked)
-                {
-                    Clipboard.SetText(posible_words[0]);
-                }
-
-                int digits = posible_words.Count.ToString().Length;
-
-                for (int i = 0; i < posible_words.Count; i++)
-                {
-
-                    int x = 0;
-                    int fontsize = int.Parse(FontSizeBox.Text);
-                    int y = i * (fontsize + 3);
-
-                    string space = new string(' ', (digits - (i + 1).ToString().Length) * 2);
-                    MyLabel mylabel = new MyLabel((i + 1) + ". " + space + posible_words[i], x, y, fontsize);
-                    LabelGrid.Children.Add(mylabel.label);
-
-                    MyButton copybutton = new MyButton("Copy " + (i + 1) + ".", 30, y, fontsize, ClickCopyButton, posible_words[i]);
-
-                    LabelGrid.Children.Add(copybutton.button);
+                    if (G == 255)
+                    {
+                        R = (int)(cooldown / 1500.0 * 255.0);
+                    }
+                    else
+                    {
+                        G = (int)((1.0 - cooldown / 3000.0) * 2 * 255.0);
+                    }
                 }
             }
         }
